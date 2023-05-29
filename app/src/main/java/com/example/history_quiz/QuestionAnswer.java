04391c2f923d3f9410d81b98201c2aa63d609662
georakom <georakom@csd.auth.gr>
2023-05-29 19:44:30 +0300

    Questions-answers added

---
+++ app/src/main/java/com/example/history_quiz/QuestionAnswer.java
@@ -0,0 +1,76 @@
+package com.example.history_quiz;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "In which year did the Armenian Genocide occur?",
+            "World War II started when Germany invaded which country?",
+            "What does NATO stand for?",
+            "Which of these best describes blitzkrieg, which was key to Germany’s early success in the World War II?",
+            "The death of which Soviet leader in 1953 brought a temporary relaxation of Cold War tensions?",
+            "First Balkan war: Which country declared war on Turkey on 9 October?",
+            "When did the World War I officially end?",
+            "When did Napoleon become the emperor of France?",
+            "In between which countries did the Falklands war occur in 1982?",
+            "The unsuccessful Soviet blockade of which city marked an early victory for the USA in the Cold War?",
+            "Which of these countries is not a part of the CSTO (Collective Security Treaty Organization)?",
+            "The Cold War turned hot in 1950 when Soviet-backed troops from this country invaded South Korea:",
+            "Which is the date of the Russian invasion in Ukraine in 2022?",
+            "What civilization is considered as the oldest in the world?",
+            "Where did the Industrial Revolution first break through?",
+            "When did Christopher Columbus discover America?",
+            "Which of the following treaties ended World War I?",
+            "What was the name of the first atom bomb dropped on the Japanese city of Hiroshima?",
+            "The first Emperor of Rome was:",
+            "Which was the year of America's independence?"
+
+
+    };
+
+    public static String choices[][] ={
+            {"1912","1915","1921","1918"},
+            {"Belgium","Soviet Union","France","Poland"},
+            {"North Atlantic Treaty Organization","Northern Alliance of Territories & Organizations","New Accord on the Terms of Occupation","North American Treaty Office"},
+            {"Economic sanctions","Quick coordinated attacks","Propaganda","Huge number of military personnel"},
+            {"Vladimir Lenin","Joseph Stalin","Nikita Khrushchev","Georgy Malenkov"},
+            {"Greece","Serbia","Bulgaria","Montenegro"},
+            {"9/3/1917","15/9/1920","11/11/1918","20/12/1917"},
+            {"1804","1810","1800","1808"},
+            {"Argentina-France","United Kingdom-France","Argentina-United Kingdom","Italy-France"},
+            {"Vienna","Warsaw","Bucharest","Berlin"},
+            {"Belarus","Armenia","Russia","Turkey"},
+            {"China","Japan","North Korea","Vietnam"},
+            {"1 March","24 February","10 March","13 February"},
+            {"Ancient Greek","Ancient Egyptian","The Babylonians","Mesopotamian"},
+            {"England","Germany","France","Italy"},
+            {"1380","1400","1297","1492"},
+            {"Treaty of Versailles","The Paris Peace Treaties","The Geneva Conference","The Peace of Westphalia"},
+            {"Destroyer","Big boy","Little boy","Scorpion"},
+            {"Augustus","Tiberius","Nero","Lucius Verus"},
+            {"1790","1750","1800","1776"}
+
+    };
+
+    public static String answer[] ={
+            "1915",
+            "Poland",
+            "North Atlantic Treaty Organization",
+            "Quick coordinated attacks",
+            "Joseph Stalin",
+            "Montenegro",
+            "11/11/1918",
+            "1804",
+            "Argentina-United Kingdom",
+            "Berlin",
+            "Turkey",
+            "North Korea",
+            "24 February",
+            "Mesopotamian",
+            "England",
+            "1492",
+            "Treaty of Versailles",
+            "Little boy",
+            "Augustus",
+            "1776"
+
+    };
+}
