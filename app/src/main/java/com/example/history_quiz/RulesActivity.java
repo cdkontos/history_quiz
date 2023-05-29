d0ec0b27c6524d0691eb5fddbc7a6cb6431f3ee9
lchristof <lchristof@csd.auth.gr>
2023-05-29 20:41:54 +0300

    Added new activities.

--- app/src/main/java/com/example/history_quiz/RulesActivity.java
+++ app/src/main/java/com/example/history_quiz/RulesActivity.java
@@ -7,19 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class RulesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final Button playButton = findViewById(R.id.playButton);
+        setContentView(R.layout.activity_rules);
+        final Button playButton = findViewById(R.id.playButton2);
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, RulesActivity.class));
+                startActivity(new Intent(RulesActivity.this, QuizActivity.class));
             }
         });
     }
-
 }
